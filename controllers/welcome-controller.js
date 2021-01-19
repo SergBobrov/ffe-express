b292cfba77c3a844d14842ea1005244ac7e908ec
SergBobrov <serggbobrov@gmail.com>
2021-01-20 01:48:11 +0300

    static html

--- controllers/welcome-controller.js
+++ controllers/welcome-controller.js
@@ -1,3 +1,7 @@
-export const welcomeController = (req, res) => {
-
-}
+//
+// export const welcomeController = (req, res) => {
+//     // res.sendFile('index2.html')
+//     // res.send('123')
+//
+//     res.render('index2.html')
+// }
